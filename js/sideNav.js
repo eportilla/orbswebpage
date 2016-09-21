484fd6bc406ca3ecfc09b1d262939dd33d4e1ab6
Erika Portilla <ewi_mp@McMac.local>
2016-09-21 00:50:14 -0700

    Updated the layout. Added the background image and fonts. Changed the Social media icons.

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,11 +1,23 @@
-function openNav() {
-    document.getElementById("sideNavLeft").style.width = "20%";
+function openLeftnav() {
+    document.getElementById("sideNavleft").style.width = "20%";
     document.getElementById("main").style.marginLeft = "20%";
-    document.getElementById("collapsebtn").style.visibility = "hidden";
+    document.getElementById("collapseLeftbtn").style.visibility = "hidden";
 }
 
-function closeNav() {
-    document.getElementById("sideNavLeft").style.width = "0";
+function closeLeftnav() {
+    document.getElementById("sideNavleft").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("collapsebtn").style.visibility = "visible";
+    document.getElementById("collapseLeftbtn").style.visibility = "visible";
+}
+
+function openRightnav() {
+    document.getElementById("sideNavright").style.width = "20%";
+    document.getElementById("main").style.marginRight = "20%";
+    document.getElementById("collapseRightbtn").style.visibility = "hidden";
+}
+
+function closeRightnav() {
+    document.getElementById("sideNavright").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.getElementById("collapseRightbtn").style.visibility = "visible";
 }
