6fdd5b0e3b03921979ce7a73f1e781f60fca9b5d
Erika Portilla <ewi_mp@McMac.local>
2016-09-24 13:06:22 -0700

    Updated the nav bars and buttons glow.

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,19 +1,19 @@
 function openLeftnav() {
-    document.getElementById("sideNavleft").style.width = "20%";
-    document.getElementById("open-left-menu").style.width = "0";
+	document.getElementById("open-left-menu").style.width = "0";
+	document.getElementById("sideNavleft").style.width = "20%";
 }
 
 function closeLeftnav() {
-    document.getElementById("sideNavleft").style.width = "0";
-    document.getElementById("open-left-menu").style.width = "20%";
+	document.getElementById("sideNavleft").style.width = "0";
+  document.getElementById("open-left-menu").style.width = "20%";
 }
 
 function openRightnav() {
-    document.getElementById("sideNavright").style.width = "20%";
-    document.getElementById("open-right-menu").style.width = "0";
+	document.getElementById("open-right-menu").style.width = "0";
+  document.getElementById("sideNavright").style.width = "20%";
 }
 
 function closeRightnav() {
-    document.getElementById("sideNavright").style.width = "0";
-    document.getElementById("open-right-menu").style.width = "20%";
+  document.getElementById("sideNavright").style.width = "0";
+  document.getElementById("open-right-menu").style.width = "20%";
 }
